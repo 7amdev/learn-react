102b94607ae8942b36acd751569e13d24b53d49b
7amdev <amonteiro.alfredo@gmail.com>
2023-07-03 10:13:10 -0100

    feature(weather-app): Add router navigation link and navigation sections

--- 07_routing/weather-app/src/App.js
+++ 07_routing/weather-app/src/App.js
@@ -1,20 +1,32 @@
-import React from "react";
-import {Route, Routes} from "react-router-dom"
+import React, { Fragment } from "react";
+import {Routes, Route, Link} from "react-router-dom"
 
 import Home from "./Home";
 import About from "./About";
+import Footer from "./Footer";
 import './app.css';
-
-
 
 const App = function () {
     return (
-        <main>
-            <Routes>
-                <Route path="/" element={ <Home /> } />
-                <Route path="/about" element={ <About /> } />
-            </Routes>
-        </main>
+        <Fragment>
+            <nav aria-label="primary">
+                <ul>
+                    <li>
+                        <Link to={"/"}>Home</Link>
+                    </li>
+                    <li>
+                        <Link to={"about"}>About</Link>
+                    </li>
+                </ul>
+            </nav>
+            <main>
+                <Routes>
+                    <Route path="/" element={ <Home /> } />
+                    <Route path="/about" element={ <About /> } />
+                </Routes>
+            </main>
+            <Footer />
+        </Fragment>
     );
 };
 
