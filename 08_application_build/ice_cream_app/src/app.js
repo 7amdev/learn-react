40636776d7c8b1f08a67344612d3870782981d08
7amdev <amonteiro.alfredo@gmail.com>
2023-07-13 09:17:54 -0100

    feature(ice-cream-app): message label for empty menu response from the server

--- 08_application_build/ice_cream_app/src/app.js
+++ 08_application_build/ice_cream_app/src/app.js
@@ -7,30 +7,13 @@ import IceCreamMenu from "./ice_cream_menu";
 import './styles/app.css';
 
 function App() {
-  const [menu, set_menu] = useState([]);
-
-  useEffect(function () {
-    fetch('/api/stock?include=ice-creams')
-      .then(function (response) {
-        if (!response.ok) {
-          throw new Error("Error: failed to load resources from the server");
-        } 
-
-        return response.json();
-      })
-      .then(function (menu_data) {
-        set_menu(menu_data);
-      })
-      .catch(function (error) {
-        console.warn(error);
-      });
-  }, []);
+  
 
   return (
     <>
       <Header />
         <main>
-          <IceCreamMenu data={menu} />
+          <IceCreamMenu />
         </main>
       <Footer />
     </>
--- 08_application_build/ice_cream_app/src/ice_cream_menu.js
+++ 08_application_build/ice_cream_app/src/ice_cream_menu.js
@@ -1,17 +1,40 @@
 import React, { useState, useEffect } from "react";
 
-const IceCreamMenu = function ({ data }) {
+const IceCreamMenu = function () {
+  const [menu, set_menu] = useState([]);
+  const [loading, set_loading] = useState(true);
 
-  console.log(data);
+  useEffect(function () {
+    fetch('/api/stock?include=ice-creams')
+      .then(function (response) {
+        if (!response.ok) {
+          throw new Error("Error: failed to load resources from the server");
+        } 
+
+        return response.json();
+      })
+      .then(function (menu_data) {
+        set_loading(function (previous_value) {
+          return !previous_value;
+        });
+        set_menu([]);
+      })
+      .catch(function (error) {
+        console.warn(error);
+      });
+  }, []);
+
   return (
     <section>
       <h2>Rock your taste buds with one of these!</h2>
       <ul>
-        {
-          data.map(item => {
+        { menu && menu.length === 0 && <li><p>No Menu Avaible today.</p></li> }
+        { menu && menu.length > 0 &&  
+          menu.map(item => {
             return (
               <li key={item.id.toString()}>
                 <section>
+                  <img src={`/ice_cream_images/ice_cream_${item.id}.jpg`} loading="lazy" />
                   <h3>{ item.ice_cream.name }</h3>
                   <p>{`$${item.price} · ${item.quantity} in stock`}</p>
                   <p>{ item.description }</p>
