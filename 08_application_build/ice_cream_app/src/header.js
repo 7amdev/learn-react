b0b8fcc873d2e2d763df10533cec1485b7babc4e
7amdev <amonteiro.alfredo@gmail.com>
2023-07-24 14:53:12 -0100

    (ice-cream-app) Add the main navigation

--- 08_application_build/ice_cream_app/src/header.js
+++ 08_application_build/ice_cream_app/src/header.js
@@ -1,12 +1,43 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import logo from './assets/logo.svg';
 import './styles/header.css';
 
 const Header = function () {
   return (
     <header className="header">
-        <img className="header__logo" src={logo} alt="App logo" />
-        <h1>Awesome Ice Cream</h1>
+      <div className="header__content">
+        <a href="#" className="logo">
+          <img className="logo__img" src={logo} alt="" />
+          <h1 className="logo__title">Awesome Ice Cream</h1>
+        </a>
+      
+        <nav className="nav">
+          <ul className="nav__list">
+            <li className="nav__list-item">
+              <NavLink 
+                to={'/'}
+                className={function ({isActive}) {
+                  console.log(isActive);
+                  if (!isActive) return 'nav__link';
+                  return 'nav__link nav__link--active';
+                }}>
+                  Menu
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+              to={'/settings'} 
+                className={function ({isActive}) {
+                  if (!isActive) return 'nav__link';
+                  return 'nav__link nav__link--active';
+                }}>
+                  Settings
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 };
