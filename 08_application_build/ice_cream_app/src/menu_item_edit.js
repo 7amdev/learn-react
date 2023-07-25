2503b180e0990cf1473216a04c86b99bd4862e3b
7amdev <amonteiro.alfredo@gmail.com>
2023-07-25 16:07:26 -0100

    (ice-cream-app) Add fetch data from the server and reditrection error for 404 response

--- 08_application_build/ice_cream_app/src/menu_item_edit.js
+++ 08_application_build/ice_cream_app/src/menu_item_edit.js
@@ -1,9 +1,73 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
+import { useParams, useNavigate } from "react-router-dom"
 import { Helmet } from "react-helmet";
+// todo
+// [] fetch data from the server
+// [] create a form
+// [] style the page
 
 const MenuItemEdit = function () {
-  return (
+  const [menu_item, set_menu_item] = useState({});
+  const navigate = useNavigate();
+  const { current: abort_controller } = useRef(new AbortController());
+  const { id } = useParams();
 
+  useEffect(function () {
+    // On desmount run the function bellow
+    return function () {
+      abort_controller.abort();  
+    };
+  }, []);
+
+  useEffect(function () {
+    const request = new Request(`/api/stock/${id}?include=ice-creams `, {
+      method: 'GET',
+      signal: abort_controller.signal
+    });
+
+    fetch(request)
+      .then(function (response) {
+        if (!response.ok) {
+          throw new Error(
+            'Error: failed to load resources from the server', {
+              cause: response
+            }
+          );
+        }
+        return response.json();
+      })
+      .then(function (data) {
+        console.log({
+          ...data,
+          price: data.price.toFixed(2),
+          quantity: data.quantity.toString()
+        });
+        set_menu_item(data);
+      })
+      .catch(function (error) {
+        console.warn(error);
+        if (error.cause && error.cause.status) {
+          switch (error.cause.status) {
+            case 400: 
+              console.warn('400 error');
+              break;
+            case 401:
+              console.warn('401 error'); 
+              break;
+            case 404: 
+              navigate(`/`);
+              console.warn('404 error');
+              break;
+            case 500: 
+              console.warn('500 error');
+              break;
+          }
+        }
+      });
+    
+  }, [useParams().id]);
+
+  return (
     <main>
       <Helmet>
         <title>
