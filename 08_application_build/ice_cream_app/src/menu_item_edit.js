4485797d371aa1c367223ee48ec7ad52d72babe1
7amdev <amonteiro.alfredo@gmail.com>
2023-07-27 15:15:31 -0100

    (ice-cream-app) Attach loaded data to the form inputs, add in-stock and quantity basic validation, unique id for labels and inputs, and more

--- 08_application_build/ice_cream_app/src/menu_item_edit.js
+++ 08_application_build/ice_cream_app/src/menu_item_edit.js
@@ -6,11 +6,36 @@ import { Helmet } from "react-helmet";
 // [] create a form
 // [] style the page
 
+const INITIAL_STATE = {
+  id: '', 
+  in_stock: false,
+  price: 0,
+  quantity: 0,
+  ice_cream: { name: '' },
+  description: ''
+};
+
+const generate_uid = function () {
+  const seed = 'abcdefghijklmnopqrstuvwxyz01234567890!@#$%&-_+=';
+  const upper_bound = seed.length;
+  const lower_bound = 0;
+  const N = 7;
+  let uid = '';
+
+  for (let i = 0; i < N; i += 1) {
+      const seed_idx = Math.random() * (upper_bound - lower_bound) + lower_bound;
+      uid += seed.charAt(seed_idx);
+  }
+
+  return uid;
+} 
+
 const MenuItemEdit = function () {
-  const [menu_item, set_menu_item] = useState({});
+  const [menu_item, set_menu_item] = useState(INITIAL_STATE);
   const navigate = useNavigate();
   const { current: abort_controller } = useRef(new AbortController());
   const { id } = useParams();
+  let   {current: input_uid} = useRef({});
 
   useEffect(function () {
     // On desmount run the function bellow
@@ -37,15 +62,18 @@ const MenuItemEdit = function () {
         return response.json();
       })
       .then(function (data) {
-        console.log({
+        const data_keys = Object.keys(data);
+        for (let i = 0; i < data_keys.length; i++) {
+          input_uid[data_keys[i]] = generate_uid();
+        }
+        set_menu_item({
           ...data,
           price: data.price.toFixed(2),
           quantity: data.quantity.toString()
         });
-        set_menu_item(data);
       })
       .catch(function (error) {
-        console.warn(error);
+        // console.warn(error);
         if (error.cause && error.cause.status) {
           switch (error.cause.status) {
             case 400: 
@@ -67,6 +95,36 @@ const MenuItemEdit = function () {
     
   }, [useParams().id]);
 
+  const on_change_handler = function (e) {
+    let new_menu_item = {...menu_item};
+    let value = e.target.value;
+
+    if (e.target.type === 'checkbox') {
+      value = e.target.checked;
+    }
+  
+    if (e.target.name === 'in_stock' && value === false ) {
+      new_menu_item = {
+        ...new_menu_item,
+        quantity: '0'
+      };
+    }
+
+    new_menu_item = {
+      ...new_menu_item,
+      [e.target.name]: value
+    };
+
+    set_menu_item(new_menu_item);
+  };
+
+  const on_form_submit_handler = function (e) {
+    e.preventDefault();
+    console.log(menu_item);
+  };
+
+  console.log(input_uid);
+
   return (
     <main>
       <Helmet>
@@ -75,6 +133,57 @@ const MenuItemEdit = function () {
         </title>
       </Helmet>
       <h2>Edit Menu Item Page</h2>
+      <form className="form" onSubmit={on_form_submit_handler}>
+        <dl>
+          <dt className="form__title">{ menu_item.ice_cream.name }</dt>
+          <dd className="form__description">{ menu_item.description }</dd>
+        </dl>
+
+        <label 
+          htmlFor={input_uid.in_stock} 
+          className="form__label">
+            In stock? 
+        </label>
+        <input 
+          id={input_uid.in_stock}
+          type="checkbox"
+          name="in_stock"
+          checked={menu_item.in_stock}
+          onChange={on_change_handler}/>
+
+        <label 
+          htmlFor={input_uid.price} 
+          className="form__label">
+            Price: 
+        </label>
+        <input 
+          id={input_uid.price}
+          type="number"
+          name="price"
+          value={menu_item.price}
+          step={".01"}
+          onChange={on_change_handler}/>
+
+        <label 
+          htmlFor={input_uid.quantity} 
+          className="form__label">
+            Quantity: 
+        </label>
+        <select 
+          id={input_uid.quantity}
+          name="quantity" 
+          value={menu_item.quantity}
+          onChange={on_change_handler}>
+          <option>0</option>
+          <option>10</option>
+          <option>20</option>
+          <option>30</option>
+          <option>40</option>
+          <option>50</option>
+        </select>
+
+        <input className="form__submit" type="submit" value={"Save"}/>
+      </form>
     </main>
   );
 };
